5351e3b0eebc93e5da2807dce8997b8735e50528
tenndawg <nickolas.tenney@gmail.com>
2017-09-07 15:09:06 -0600

    no worky step 6

--- src/components/List/CreateCustomer/CreateCustomer.js
+++ src/components/List/CreateCustomer/CreateCustomer.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './CreateCustomer.css';
 
-export default function createCustomer({startNewCustomer}) {
+export default function CreateCustomer({startNewCustomer}) {
+  create() {
+    const { first, last, email, phone } = this.state;
+    var customer = {
+      first,
+      last,
+      email,
+      phone,
+      status: 'New Customer',
+      log: ''
+    }
+  
+    this.props.createCustomer( customer );
+  }
    return (
     <div id="CreateCustomerBtn__container">
       <button id="CreateCustomer__btn" onClick={startNewCustomer}> New Customer </button>
--- src/components/Workspace/Workspace.js
+++ src/components/Workspace/Workspace.js
@@ -29,7 +29,7 @@ function Workspace( { initialLoad, creating, currentCustomer, createCustomer } )
                         status={currentCustomer.status}
                         log={currentCustomer.log}
                         />
-            : null
+                : null
       }
     </div>
   )
